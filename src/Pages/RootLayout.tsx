1a087aa431c187654a728ecdcd0cd4dd4675ead1
David Sotto Mayor <ddsmmf@gmail.com>
2024-04-08 20:26:32 -0400

    Marking the RootLayout

--- src/Pages/RootLayout.tsx
+++ src/Pages/RootLayout.tsx
@@ -1,13 +1,31 @@
 import React, { Fragment } from 'react'
 import MainNavigation from '../components/MainNavigation'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import Home00WeBSVmenu from './Home00WeBSVmenu';
 
 const RootLayout = () => {
+  let location = useLocation();
+  console.log(location.pathname)
+
+  const outletReturn = (
+    <div style={{'display': 'flex', 'justifyContent': 'space-between', 'background': 'green', 'width': '100vw'}}>
+      <div style={{'background': 'red','width': '50%'}}>
+        oiiiiiiiiii
+      </div>
+      <Outlet/>
+      <div style={{'background': 'red','width': '50%'}}>
+        oiiiiiiiiii
+      </div>
+    </div> 
+  )
+
   return (
     <Fragment>
         <MainNavigation/>
         <main style={{padding: '0.5%'}}>
-            <Outlet/>
+          {
+            location.pathname === '/' ? <Home00WeBSVmenu/> : outletReturn
+          }
         </main>
     </Fragment>
   )
