53db98d42616b902680ab1ea0ca2733b2c92c69b
Poliana Tome <ptome8727@gmail.com>
2024-04-17 21:47:14 -0400

    Incluindo alterações e adição de imagens

--- src/Pages/Home00WeBSVmenu.tsx
+++ src/Pages/Home00WeBSVmenu.tsx
@@ -1,119 +1,72 @@
-// src/components/Home.tsx
-import React, {FC} from 'react';
-import { useState, useRef, useEffect } from "react";
-import { DefaultProvider, sha256, toHex, PubKey, bsv, TestWallet, Tx, toByteString } from "scrypt-ts";
+import React, { FC, useState, useEffect } from 'react';
 import '../App.css';
 
-export let homepvtKey: string = "";
-export let homenetwork = bsv.Networks.testnet;
-export let compState = true;
-
-
 const Home00WeBSVmenu: FC = () => {
-
   const texts = [
-    'Home/New PVT Key:', 'Create a new Private Key to Navegate',
-    'Home/Access:', 'Use a password or private key to Navegate',
+    'Home/New PVT Key:', 'Create a new Private Key to Navigate',
+    'Home/Access:', 'Use a password or private key to Navigate',
     'Home/Didactic:', 'Learn some didactic functions',
-    'Satoshi to Peer/Send Satoshi:', 'Transfer satoshis with standart scripts P2PKH and P2PK',
-    'Satoshi to Peer/Data on Chain:', 'Write or Retrive chain data',
+    'Satoshi to Peer/Send Satoshi:', 'Transfer satoshis with standard scripts P2PKH and P2PK',
+    'Satoshi to Peer/Data on Chain:', 'Write or Retrieve chain data',
     'Satoshi to Peer/Data Token R:', 'Create-Reshape-Transfer-Melt OP_RETURN data Token',
     'Satoshi to Peer/Data Token D:', 'Create-Reshape-Transfer-Melt OP_DROP data Token',
     'Satoshi to Peer/nSatOrdinals:', 'Create-Reshape-Transfer-Melt 1SatOrdinals or nSatOrdinals',
-    'Satoshi to Peer/UTXO list:', 'Get UTXO list of an Address or Scrip Hash',
+    'Satoshi to Peer/UTXO list:', 'Get UTXO list of an Address or Script Hash',
     'Smart Contracts:', 'Coming Soon!!!'
-  
   ];
 
   const [textIndex, setTextIndex] = useState(0);
   const [labelText, setLabelText] = useState(texts[0]);
   const [labelText2, setLabelText2] = useState(texts[1]);
+  const [imageSize, setImageSize] = useState(300); // Inicialize com o tamanho desejado da imagem
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      //const newIndex = (textIndex + 1) % texts.length;
       const newIndex = ((textIndex + 2)) % (texts.length);
       setLabelText(texts[newIndex]);
       setLabelText2(texts[newIndex + 1]);
       setTextIndex(newIndex);
-    }, 4000); // Change text every 4 seconds
+    }, 4000); // Altera o texto a cada 4 segundos
 
-    // Clear the interval when the component unmounts
+    // Limpa o intervalo quando o componente é desmontado
     return () => clearInterval(intervalId);
   }, [textIndex]);
 
-
   return (
-
-    <div >
+    <div>
       <div className="App-header2">
-        <h2 style={{ fontSize: '30px', paddingBottom: '5px', paddingTop: '5px'}}>
-
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-          
-          Smart 1SatOrdinals
-          {/*
-          BlockChain Lightning Web Menu
-          Satoshi Vision Web Menu
-          Bitcoin Lightning Network
-          Bitcoin Lightning Web Menu
-          */}
-          
+        <h2 style={{ fontSize: '30px', paddingBottom: '5px', paddingTop: '5px' }}>
+          Smart Ordinals
         </h2>
       </div>
-
-
-      <div className="body2">
-
-        {//<h1>Embedded External Website</h1>
-        }
-
+      <div className="body2" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <div style={{ textAlign: 'center', paddingTop: '20px', paddingBottom: '20px' }}>
-                
-                <label style={{ fontSize: '20px', paddingBottom: '2px' }}
-                  >Enjoy Navegating  
-                </label>     
+          <label style={{ fontSize: '20px', paddingBottom: '2px' }}>
+            Enjoy Navigating
+          </label>
         </div>
-
-        <iframe
-          title="External Website"
-          src="https://makc.github.io/misc/relativity-1.html" // Replace with the URL of the website you want to display
-          width="100%"
-          height="200px" // Set the height as needed
-          
-          allowFullScreen
-        ></iframe>
-
+        <div style={{ textAlign: 'center' }}>
+          <img src="/engcima.png" alt="Descrição da imagem" style={{ width: `${imageSize}px`, height: 'auto' }} />
+        </div>
+        <div style={{ textAlign: 'center', paddingTop: '20px', paddingBottom: '20px' }}>
+          <label style={{ fontSize: '14px', paddingBottom: '2px' }}>
+            {labelText}
+          </label>
+        </div>
+        <div style={{ textAlign: 'center', paddingTop: '20px', paddingBottom: '40px' }}>
+          <label style={{ fontSize: '14px', paddingBottom: '2px' }}>
+            {labelText2}
+          </label>
+        </div>
+        <a href='https://medium.com/@cktcracker/websvmenu-faac499d0da5' target="_blank" rel="noopener noreferrer" style={{ fontSize: '14px', paddingBottom: '5px', color: 'yellow' }}>
+          About websvmenu
+        </a>
+        {/* Adicione controles para ajustar o tamanho da imagem */}
         <div>
-
-          <div style={{ textAlign: 'center', paddingTop: '20px', paddingBottom: '20px' }}>
-                
-                <label style={{ fontSize: '14px', paddingBottom: '2px' }}
-                  > 
-                  {labelText}  
-                </label>     
-
-          </div>
-
-          <div style={{ textAlign: 'center', paddingTop: '20px', paddingBottom: '40px' }}>
-                
-                <label style={{ fontSize: '14px', paddingBottom: '2px' }}
-                  > 
-                  {labelText2}  
-                </label>     
-          </div>
-
-          <a href='https://medium.com/@cktcracker/websvmenu-faac499d0da5' target="_blank" rel="noopener noreferrer"
-          style={{ fontSize: '14px', paddingBottom: '5px', color: 'yellow' }}>
-            About websvmenu
-          </a>
-
+          <input type="range" min="100" max="500" value={imageSize} onChange={(e) => setImageSize(parseInt(e.target.value))} />
         </div>
-
       </div>
-
     </div>
-
   );
 };
 
--- src/Pages/RootLayout.tsx
+++ src/Pages/RootLayout.tsx
@@ -1,34 +1,42 @@
-import React, { Fragment } from 'react'
-import MainNavigation from '../components/MainNavigation'
-import { Outlet, useLocation } from 'react-router-dom'
+
+
+import React, { Fragment } from 'react';
+import MainNavigation from '../components/MainNavigation';
+import { Outlet, useLocation } from 'react-router-dom';
 import Home00WeBSVmenu from './Home00WeBSVmenu';
+
 
 const RootLayout = () => {
   let location = useLocation();
-  console.log(location.pathname)
+  console.log(location.pathname);
 
   const outletReturn = (
-    <div style={{'display': 'flex', 'justifyContent': 'space-between', 'background': 'green', 'width': '100vw'}}>
-      <div style={{'background': 'red','width': '50%'}}>
-        oiiiiiiiiii
+    <div style={{ display: 'flex', justifyContent: 'space-between', background: '#556B2F', width: '50vw' }}>
+      <div style={{ height: '90vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+          <img src="/img_proj.png" alt="Descrição da imagem" />
+        </div>
       </div>
-      <Outlet/>
-      <div style={{'background': 'red','width': '50%'}}>
-        oiiiiiiiiii
+      
+      <Outlet />
+      <div style={{ display: 'flex', justifyContent: 'space-between', background: '#556B2F', width: '50vw' }}>
+        <div style={{ height: '90vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <div>
+            <img src="/img_proj.png" alt="Descrição da imagem" />
+          </div>
+        </div>
       </div>
-    </div> 
-  )
+    </div>
+  );
 
   return (
     <Fragment>
-        <MainNavigation/>
-        <main style={{padding: '0.5%'}}>
-          {
-            location.pathname === '/' ? <Home00WeBSVmenu/> : outletReturn
-          }
-        </main>
+      <MainNavigation />
+      <main style={{ padding: '0.5%' }}>
+        {location.pathname === '/' ? <Home00WeBSVmenu /> : outletReturn}
+      </main>
     </Fragment>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
